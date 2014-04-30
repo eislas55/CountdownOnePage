adf559bd07c483ad2e436d41236a834b83b846e7
eislas55 <eislas55@gmail.com>
2014-04-30 13:35:35 -0500

    30 abr 1:35pm

--- js/jquery.countdown.js
+++ js/jquery.countdown.js
@@ -112,9 +112,7 @@ and GPL-3.0 (http://opensource.org/licenses/GPL-3.0) licenses.
       refresh: 1000,
       onEnd: $.noop,
       render: function(date) {
- //       return $(this.el).html("" + date.years + " Años, " + date.days + " Dias, " + (this.leadingZeros(date.hours)) + " Horas, " + (this.leadingZeros(date.min)) + " Minutos y " + (this.leadingZeros(date.sec)) + " Segundos");
-        return $(this.el).html("<div class=\"clockStyle\">" + date.years + "<br/>Años</div><div class=\"clockStyle\">" + date.days + "<br/>Dias</div><div class=\"clockStyle\">" + (this.leadingZeros(date.hours)) + "<br/>Horas</div><div class=\"clockStyle\">" + (this.leadingZeros(date.min)) + "<br/>Minutos</div><div class=\"clockStyle\">" + (this.leadingZeros(date.sec)) + "<br/>Segundos</div>");
-//  return $(this.el).html("<div class=\"clockStyle\">"+ date.years);
+        return $(this.el).html("</div><div class=\"clockStyle\">" + date.days + "<br/>Dias</div><div class=\"clockStyle\">" + (this.leadingZeros(date.hours)) + "<br/>Horas</div><div class=\"clockStyle\">" + (this.leadingZeros(date.min)) + "<br/>Minutos</div><div class=\"clockStyle\">" + (this.leadingZeros(date.sec)) + "<br/>Segundos</div>");
       }
     };
     $.fn.countdown = function(options) {
